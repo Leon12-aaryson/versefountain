53bbba42a0941c628cda54d35b13a06052254658
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2025-05-30 15:51:16 +0300

    authenticate the backend for user creation and login

--- routes/api.php
+++ routes/api.php
@@ -28,7 +28,8 @@ Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])->
 Route::post('/reset-password', [NewPasswordController::class, 'store'])->middleware('guest')->name('password.store');
 Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)->middleware(['auth', 'signed', 'throttle:6,1'])->name('verification.verify');
 Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])->middleware(['auth', 'throttle:6,1'])->name('verification.send');
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth')->name('logout');
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->middleware('auth:sanctum');
 
 use Illuminate\Support\Facades\Auth;
 
